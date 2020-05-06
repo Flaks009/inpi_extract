1b02974c144109c9421915e56bb969e73bc788f7
flaks009 <flaks009@gmail.com>
2020-05-06 18:50:52 -0300

    fix path

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-wget http://revistas.inpi.gov.br/txt/$1$2.zip --directory-prefix=revistas/
+wget http://revistas.inpi.gov.br/txt/$1$2.zip --directory-prefix=/home/ubuntu/inpi_extract/revistas/
 
-unzip revistas/$1$2.zip -d revistas/
+unzip revistas/$1$2.zip -d /home/ubuntu/inpi_extract/revistas/
 
 sleep 3
