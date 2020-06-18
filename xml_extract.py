9dbe7fd5589c2c6370b5747f4d527c8626acbbb9
flaks009 <flaks009@gmail.com>
2020-06-18 20:05:30 -0300

    marca col order

--- xml_extract.py
+++ xml_extract.py
@@ -5,6 +5,29 @@ import pandas as pd
 def get_marca(nome_revista):
     tree = ET.parse('/home/ubuntu/inpi_extract/revistas/'+nome_revista+'.xml')
     root = tree.getroot()
+    columns = {
+        'uf',
+        'codigo',
+        'pais',
+        'numero_rpi',
+        'edicao',
+        'data-recebimento-inpi',
+        'data-vigencia',
+        'numero-inscricao-internacional',
+        'nome-razao-social',
+        'nome',
+        'data-concessao',
+        'procurador',
+        'marca',
+        'data-deposito',
+        'processo',
+        'apresentacao',
+        'natureza',
+        'numero',
+        'codigoServico',
+        'data'}
+    df = pd.DataFrame(columns = columns)
+
 
     list_proc = []
     dict_attrib = {}
@@ -19,8 +42,7 @@ def get_marca(nome_revista):
                     dict_attrib.update({b.tag : b.text})
             dict_attrib.update({'numero_rpi':nome_revista[2:]})
             list_proc.append(dict_attrib)
-    
-    df = pd.DataFrame(list_proc)
 
-    df.to_excel('/home/ubuntu/inpi_extract/xlsx/{}.xlsx'.format(nome_revista[:6]))
-    df.to_csv('/home/ubuntu/inpi_extract/xlsx/{}.csv'.format(nome_revista[:6]), index=False, sep=';')
+    df = df.append(list_proc)
+
+    df.to_excel('/home/ubuntu/inpi_extract/xlsx/{}.xlsx'.format(nome_revista[:6]), index=False, sep=";")
